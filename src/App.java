d1864ae5ddfb944d599143260a02b45c118a9b4e
Rysanz <aryganteng13@gmail.com>
2022-01-11 14:10:23 +0800

    hapus library

--- src/App.java
+++ src/App.java
@@ -4,7 +4,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-import com.mysql.cj.xdevapi.Table;
 
 import javafx.application.Application;
 import javafx.collections.ObservableList;
